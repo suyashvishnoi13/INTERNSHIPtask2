7fd889fbf5e2b50709ae6fa729971075bb85fe6e
suyashvishnoi13 <23103152@mail.jiit.ac.in>
2025-10-25 13:33:09 +0530

    Deleted folder 'foldername'

--- task1/task1.js
+++
@@ -1,4 +0,0 @@
-// Adds basic interactivity
-document.getElementById("alertButton").addEventListener("click", () => {
-  alert("Hello! You clicked the button. 🎉\nWelcome to Web Development Basics!");
-});
